ed60eff47cb23a0ba65c866e6de9c81e81bd1c0c
Sardorbek3101 <oscodeer@gmail.com>
2022-12-21 20:11:12 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 #EMAIL BACKEND
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'oscodeer@gmail.com'
+EMAIL_HOST_PASSWORD = 'azeysvehobipemxz'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
